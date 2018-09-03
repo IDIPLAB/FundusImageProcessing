a64a96c4634d1fbebd1ecfa05c33ddae112ac91a
ai1361720220000 <136172022@qq.com>
2018-09-03 10:55:44 +0800

    Revert "Merge branch 'master' of https://github.com/ai1361720220000/FundusImageProcessing"
    This reverts commit 6640568602a552cc47a6b7fed113afc7bc3092f8, reversing
    changes made to 2cbb954e27a9afadcc88a2a1ca60734860ea15ff.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,7 @@ int main(int argc, char *argv[])
 	MainWindow *w=new MainWindow;
 	w->show();
 	return a.exec();
-<<<<<<< HEAD
 	//hhhh
     //dfsfsdfsdf
     //jjjjjjjjjjjjjjjjjjjjjjjjjj
-=======
-	
->>>>>>> 5d8c00ad0ae1359dfef19f2cb49141f884d58143
 }
